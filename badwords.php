42a4f369e9f8f93f2bb60774c1c5f675b91c8b3a
MiriamPecora <miriampecora97@gmail.com>
2024-06-18 16:17:16 +0200

    [UPD] Added censured text and its length to the page

--- badwords.php
+++ badwords.php
@@ -1,10 +1,11 @@
 <?php 
-
     $to_be_censured = $_GET['toBeCensured'];
+    $to_be_censured_length = strlen($to_be_censured);
 
     $badword = $_GET['badword'];
-
-    $to_be_censured_length = strlen($to_be_censured);
+    
+    $censured_text = str_replace($badword, '***', $to_be_censured);
+    $censured_text_length = strlen($censured_text);
 ?>
 
 <!DOCTYPE html>
@@ -18,5 +19,9 @@
         <h3>Testo inserito</h3>
         <p>"<?php echo $to_be_censured;?>"</p>
         <p>Numero di caratteri all'interno del paragrafo: <span style='color: blue;'><?php echo $to_be_censured_length; ?></span></p>
+        <hr>
+        <h3>Qui il testo censurato:</h3>
+        <p>"<?php echo $censured_text;?>"</p>
+        <p>Numero di caratteri all'interno del paragrafo censurato: <span style='color: blue;'><?php echo $censured_text_length; ?></span></p>
     </body>
 </html>
